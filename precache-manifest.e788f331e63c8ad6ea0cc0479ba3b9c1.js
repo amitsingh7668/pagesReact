6aa352a4437ce44c7fbd198bd89d7b0043a58ff5
Amit Singh <amitsingh@Amits-MacBook-Air.local>
2021-05-27 21:54:36 +0530

    done

---
+++ precache-manifest.e788f331e63c8ad6ea0cc0479ba3b9c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "312cd329381eb825e3bea5f84a4e73e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ca3c3cda56da6a43112",
+    "url": "/static/css/main.0bade2a4.chunk.css"
+  },
+  {
+    "revision": "497b8e7e471f0ceeb43d",
+    "url": "/static/js/2.a45eaed0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a45eaed0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ca3c3cda56da6a43112",
+    "url": "/static/js/main.eb42fd8c.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
